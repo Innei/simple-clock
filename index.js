420c1127e6a33acb26c406789e70d008f82e7a12
Innei <tukon479@gmail.com>
2022-01-02 13:36:58 +0800

    feat: update style

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-/*
- * @Author: Innei
- * @Date: 2020-08-22 13:49:32
- * @LastEditTime: 2020-08-22 22:10:28
- * @LastEditors: Innei
- * @FilePath: /simple-clock/index.js
- * @Coding with Love
- */
-// @ts-check
 import dynamics from 'dynamics.js'
+// @ts-check
 /**
  * @type {HTMLDivElement}
  */
@@ -22,6 +14,7 @@ const $hour = document.querySelector('.hour-hand')
 const $second = document.querySelector('.second-hand')
 const animateEls = [$hour, $minute, $second]
 
+// 摆动指针
 function springHand(el, deg) {
   dynamics.animate(
     el,
@@ -31,7 +24,7 @@ function springHand(el, deg) {
     { frequency: 500, type: dynamics.spring },
   )
 }
-
+// 初始动画
 function doAnimate() {
   const $root = document.querySelector('.clock-root .point')
   dynamics.animate(
@@ -52,56 +45,62 @@ let timer
 
 function init() {
   doAnimate()
+
   const time = new Date()
-  const min = time.getMinutes()
-  const s = time.getSeconds()
+  const minute = time.getMinutes()
+  const second = time.getSeconds()
   const hour = time.getHours()
 
-  let minDeg = 180 + (360 / 60) * min
-  let sDeg = 180 + (360 / 60) * s
-  let hourDeg = 180 + (360 / 12) * hour
+  let minuteDeg = 180 + (360 / 60) * minute + (360 / 3600) * second
+  let secondDeg = 180 + (360 / 60) * second
+  let hourDeg =
+    180 + (360 / 12) * hour + (360 / 720) * second + (360 / 43200) * minute
 
-  let secondsPass = 0
-  let minutesPass = 0
-  let isFirstChangeSecond = false
-  let isFirstChangeMinute = false
-
+  const $time = document.getElementById('time')
+  // 每秒动画
   function setTime() {
-    springHand($minute, minDeg)
+    springHand($minute, minuteDeg)
     springHand($hour, hourDeg)
-    springHand($second, sDeg)
+    springHand($second, secondDeg)
 
-    secondsPass++
-    sDeg += 360 / 60
-    if (secondsPass % (60 - (isFirstChangeSecond ? 0 : s)) == 0) {
-      isFirstChangeSecond = true
-      secondsPass = 0
-      minDeg += 6
-      minutesPass++
-    }
-    if (
-      minutesPass &&
-      minutesPass % (60 - (isFirstChangeMinute ? 0 : min)) == 0
-    ) {
-      minutesPass = 0
-      isFirstChangeMinute = true
-      hourDeg += 360 / 12
-    }
+    secondDeg += 360 / 60
+    minuteDeg += 360 / 3600
+    hourDeg += 360 / 43200
+
+    const second = ((30 + secondDeg / 6) | 0) % 60
+    const minute = ((30 + minuteDeg / 6) | 0) % 60
+    const hour = ((6 + hourDeg / 30) | 0) % 12
+
+    $time.innerText = `${hour}:${minute.toString().padStart(2, '0')}:${second
+      .toString()
+      .padStart(2, '0')}`
   }
 
   setTime()
   timer = setInterval(setTime, 1000)
 }
+// 重置函数
+function reset() {
+  animateEls.map(($) => {
+    dynamics.stop($)
+  })
+  timer = clearInterval(timer)
+}
 document.addEventListener('visibilitychange', () => {
   if (document.visibilityState == 'hidden') {
-    animateEls.map(($) => {
-      dynamics.stop($)
-    })
-    timer = clearInterval(timer)
+    reset()
   }
 
   if (document.visibilityState == 'visible') {
     init()
   }
 })
-init()
+
+setTimeout(() => {
+  init()
+})
+// 5 分钟同步
+setTimeout(() => {
+  reset()
+  init()
+}, 300000)
